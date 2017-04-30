f8e858286cd55c16c44f26bd348267f11cde7fbf
martinock <martino.aksel.11@gmail.com>
2017-04-30 15:11:36 +0700

    App UI

--- app/src/main/java/com/martinock/facedetector/MainActivity.java
+++ app/src/main/java/com/martinock/facedetector/MainActivity.java
@@ -2,12 +2,38 @@ package com.martinock.facedetector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnBrowse;
+    private Button btnProcess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBrowse = (Button) findViewById(R.id.btn_browse);
+        btnProcess = (Button) findViewById(R.id.btn_process);
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void ProcessImage() {
+
     }
 }
